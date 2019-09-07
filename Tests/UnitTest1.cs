1aa7601ef6bb3a98d681d4c0b3057e9b941cadf2
SebaColina <sebacoli00@gmail.com>
2019-09-07 16:49:26 -0300

    primer caso de prueba para la clase Tag

--- Tests/UnitTest1.cs
+++ Tests/UnitTest1.cs
@@ -1,13 +1,29 @@
 using System;
 using Xunit;
 using ExerciseOne;
+using System.Collections.Generic;
+using Attribute = ExerciseOne.Attribute;
+
 namespace Tests
 {
-    public class UnitTest1
+    public class TagTests
     {
         [Fact]
-        public void Test1()
+        public void CreacionCorrectaDeUnTag()
         {
+            List<Attribute> listaAtributos = new List<Attribute>();
+            listaAtributos.Add(new Attribute("pedro","15"));
+            listaAtributos.Add(new Attribute("marcos","13"));
+            Tag tag = new Tag("personas",listaAtributos);
+            string NameExpected = "personas";
+            List<Attribute> ListaAtributosExpected = listaAtributos;
+            Assert.Equal(NameExpected,tag.Name);
+            Assert.Equal(ListaAtributosExpected,tag.ListaAtributos);
         }
+        //[Fact]
+        //public void Test2()
+        //{
+        //    List<Attribute> listaAtributos = new List<Attribute>();
+        //}
     }
 }
