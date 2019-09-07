f0d7af497d82f369a2644f4f4d57e677424ba473
Santiago Varaldo <santivaraldo02@gmail.com>
2019-09-07 16:20:24 -0300

    Agrego tests para la clase Attribute

--- Tests/UnitTest1.cs
+++ Tests/UnitTest1.cs
@@ -1,13 +1,28 @@
 using System;
 using Xunit;
 using ExerciseOne;
+using Attribute = ExerciseOne.Attribute;
+
 namespace Tests
 {
     public class UnitTest1
     {
         [Fact]
-        public void Test1()
+        public void ClaveInvalida()
         {
+            string expected = null;
+            string valor = "nombre";
+            Attribute attribute = new Attribute("", valor);
+            Assert.Equal(attribute.Clave, expected);
+        }
+
+        [Fact]
+        public void ValorInvalido()
+        {
+            string expected = null;
+            string clave = "clave";
+            Attribute attribute = new Attribute(clave, "");
+            Assert.Equal(attribute.Valor, expected);
         }
     }
 }
