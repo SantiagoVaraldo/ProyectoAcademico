c38e5317f2ccf478425c4cdde4de3857321563c6
Santiago Varaldo <santivaraldo02@gmail.com>
2019-09-13 21:25:46 -0300

    Formatee

--- Tests/AttributeTests.cs
+++ Tests/AttributeTests.cs
@@ -3,43 +3,43 @@ using Attribute = ExerciseOne.Attribute;
 
 namespace Tests
 {
-         public class AttributeTests
-         {
+    public class AttributeTests
+    {
 
-                  [Fact]
-                  public void InvalidKey()
-                  {
-                           string expected = null;
-                           string valor = "nombre";
-                           Attribute attribute = new Attribute("", valor);
-                           Assert.Equal(attribute.Clave, expected);
-                  }
+        [Fact]
+        public void InvalidKey()
+        {
+            string expected = null;
+            string valor = "nombre";
+            Attribute attribute = new Attribute("", valor);
+            Assert.Equal(attribute.Clave, expected);
+        }
 
-                  [Fact]
-                  public void InvalidValue()
-                  {
-                           string expected = null;
-                           string clave = "clave";
-                           Attribute attribute = new Attribute(clave, "");
-                           Assert.Equal(attribute.Valor, expected);
-                  }
-                  [Fact]
-                  public void ValidValueAndkey()
-                  {
-                           string ClaveExpected = "color";
-                           string ValorExpected = "rojo";
-                           Attribute attribute = new Attribute("color", "rojo");
-                           Assert.Equal(attribute.Valor, ValorExpected);
-                           Assert.Equal(attribute.Clave, ClaveExpected);
-                  }
-                  [Fact]
-                  public void InvalidKeyAndValue()
-                  {
-                           string ClaveExpected = null;
-                           string ValorExpected = null;
-                           Attribute attribute = new Attribute("", "");
-                           Assert.Equal(attribute.Valor, ValorExpected);
-                           Assert.Equal(attribute.Clave, ClaveExpected);
-                  }
-         }
+        [Fact]
+        public void InvalidValue()
+        {
+            string expected = null;
+            string clave = "clave";
+            Attribute attribute = new Attribute(clave, "");
+            Assert.Equal(attribute.Valor, expected);
+        }
+        [Fact]
+        public void ValidValueAndkey()
+        {
+            string ClaveExpected = "color";
+            string ValorExpected = "rojo";
+            Attribute attribute = new Attribute("color", "rojo");
+            Assert.Equal(attribute.Valor, ValorExpected);
+            Assert.Equal(attribute.Clave, ClaveExpected);
+        }
+        [Fact]
+        public void InvalidKeyAndValue()
+        {
+            string ClaveExpected = null;
+            string ValorExpected = null;
+            Attribute attribute = new Attribute("", "");
+            Assert.Equal(attribute.Valor, ValorExpected);
+            Assert.Equal(attribute.Clave, ClaveExpected);
+        }
+    }
 }
--- src/Library/Tag.cs
+++ src/Library/Tag.cs
@@ -2,43 +2,43 @@ using System.Collections.Generic;
 
 namespace ExerciseOne
 {
-         /// <summary>
-         /// NOMBRE: Tag.
-         /// DESCRIPCION: Esta clase se encarga de conocer toda la informacion pertinente a los tags.
-         /// 
-         /// PATRON EXPERT: Esta clase cumple con el patron Expert, porque es experta en conocer la informacion
-         /// pertinente para nuestros requisitos de crear objetos Tag, conoce el nombre de un Tag y una lista de
-         ///  objetos Attribute.
-         /// 
-         /// SRP: Esta clase presenta una unica responsabilidad que es conocer la informacion
-         /// de tags, su unica razon de cambio es modificar los datos que guardamos sobre tags.
-         /// 
-         /// COLABORACIONES: Colabora con la clase Attribute ya que debe conocer una lista 
-         /// de objetos Attribute.
-         /// </summary>
-         public class Tag
-         {
-                  private List<Attribute> listaAtributos = new List<Attribute>();
-                  public List<Attribute> ListaAtributos
-                  {
-                           get { return this.listaAtributos; }
-                           set { this.listaAtributos = value; }
-                  }
-                  private string name;
-                  public string Name
-                  {
-                           get { return this.name; }
-                           set { this.name = value; }
-                  }
-                  public Tag(string Name, List<Attribute> Lista_atributos)
-                  {
-                           this.Name = Name;
-                           this.ListaAtributos = Lista_atributos;
-                  }
-                  public Tag(string Name)
-                  {
-                           this.Name = Name;
-                           this.ListaAtributos = null;
-                  }
-         }
+    /// <summary>
+    /// NOMBRE: Tag.
+    /// DESCRIPCION: Esta clase se encarga de conocer toda la informacion pertinente a los tags.
+    /// 
+    /// PATRON EXPERT: Esta clase cumple con el patron Expert, porque es experta en conocer la informacion
+    /// pertinente para nuestros requisitos de crear objetos Tag, conoce el nombre de un Tag y una lista de
+    ///  objetos Attribute.
+    /// 
+    /// SRP: Esta clase presenta una unica responsabilidad que es conocer la informacion
+    /// de tags, su unica razon de cambio es modificar los datos que guardamos sobre tags.
+    /// 
+    /// COLABORACIONES: Colabora con la clase Attribute ya que debe conocer una lista 
+    /// de objetos Attribute.
+    /// </summary>
+    public class Tag
+    {
+        private List<Attribute> listaAtributos = new List<Attribute>();
+        public List<Attribute> ListaAtributos
+        {
+            get { return this.listaAtributos; }
+            set { this.listaAtributos = value; }
+        }
+        private string name;
+        public string Name
+        {
+            get { return this.name; }
+            set { this.name = value; }
+        }
+        public Tag(string Name, List<Attribute> Lista_atributos)
+        {
+            this.Name = Name;
+            this.ListaAtributos = Lista_atributos;
+        }
+        public Tag(string Name)
+        {
+            this.Name = Name;
+            this.ListaAtributos = null;
+        }
+    }
 }
