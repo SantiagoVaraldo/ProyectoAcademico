44f00974b7b056b17c63a8099db1833dbf1ab871
Santiago Varaldo <santivaraldo02@gmail.com>
2019-10-05 13:32:19 -0300

    Agrego PipeNull

---
+++ SegundaEntrega/src/Library/PipeNull.cs
@@ -0,0 +1,24 @@
+using Library;
+using ExerciseOne;
+
+
+
+namespace CompAndDel.Pipes
+{
+    public class PipeNull : IPipe
+    {
+        Tag Tag;
+
+        /// <summary>
+        ///  Recibe un tag, lo guarda en una variable Tag y la retorna.
+        /// </summary>
+        /// <param name="tag">Tag a recibir</param>
+        /// <returns>La misma imagen</returns>
+        public Tag Send(Tag tag)
+        {
+            this.Tag = tag;
+            return Tag;
+        }
+
+    }
+}
