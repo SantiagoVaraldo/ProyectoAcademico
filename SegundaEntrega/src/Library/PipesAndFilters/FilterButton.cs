2581cb956170a744c32703dc75da6955820d8a04
Sebastián de los Santos <sdelossantos19@gmail.com>
2019-10-06 01:50:52 -0300

    fix stackoverflow keynotfoundexception

--- SegundaEntrega/src/Library/PipesAndFilters/FilterButton.cs
+++ SegundaEntrega/src/Library/PipesAndFilters/FilterButton.cs
@@ -6,27 +6,28 @@ using System.IO;
 
 namespace Library
 {
-         public class FilterButton : IFilterConditional
-         {
-                  public bool Result
-                  {
-                           get {return this.Result; }
-                           set {this.Result = value;}
-                  }
-                  public Tag Filter(Tag tag)
-                  {
-                           if (tag.Name == "Button")
-                           {
-                                    this.Result = true;
-                                    Element button = new Button(tag.ListaAtributos["Name"].Valor, Int32.Parse(tag.ListaAtributos["PositionY"].Valor), Int32.Parse(tag.ListaAtributos["PositionX"].Valor), Int32.Parse(tag.ListaAtributos["Length"].Valor), Int32.Parse(tag.ListaAtributos["Width"].Valor), Creator.world.ListaLevel[Creator.world.ListaLevel.Count -1].ListaScreen[Creator.world.ListaLevel[Creator.world.ListaLevel.Count -1].ListaScreen.Count-1], tag.ListaAtributos["ImagePath"].Valor );
-                                    Creator.world.ListaLevel[Creator.world.ListaLevel.Count -1].ListaScreen[Creator.world.ListaLevel[Creator.world.ListaLevel.Count -1].ListaScreen.Count-1].Add(button);
-                           }
-                           else
-                           {
-                                    this.Result = false;
-                           }
-                           return tag;
+    public class FilterButton : IFilterConditional
+    {
+        private bool result;
+        public bool Result
+        {
+            get { return this.result; }
+            set { this.result = value; }
+        }
+        public Tag Filter(Tag tag)
+        {
+            if (tag.Name == "Button")
+            {
+                this.Result = true;
+                Element button = new Button(tag.ListaAtributos["Name"].Valor, Int32.Parse(tag.ListaAtributos["PositionY"].Valor), Int32.Parse(tag.ListaAtributos["PositionX"].Valor), Int32.Parse(tag.ListaAtributos["Length"].Valor), Int32.Parse(tag.ListaAtributos["Width"].Valor), Creator.world.ListaLevel[Creator.world.ListaLevel.Count - 1].ListaScreen[Creator.world.ListaLevel[Creator.world.ListaLevel.Count - 1].ListaScreen.Count - 1], tag.ListaAtributos["ImagePath"].Valor);
+                Creator.world.ListaLevel[Creator.world.ListaLevel.Count - 1].ListaScreen[Creator.world.ListaLevel[Creator.world.ListaLevel.Count - 1].ListaScreen.Count - 1].Add(button);
+            }
+            else
+            {
+                this.Result = false;
+            }
+            return tag;
 
-                  }
-         }
+        }
+    }
 }
