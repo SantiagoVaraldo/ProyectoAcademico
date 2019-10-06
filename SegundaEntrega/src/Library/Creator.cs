6585515ef4540727860a4e71d52f77c2eea207f8
Sebastian de los Santos <sdelossantos19@gmail.com>
2019-10-06 18:58:49 -0300

    add creator getcontent

--- SegundaEntrega/src/Library/Creator.cs
+++ SegundaEntrega/src/Library/Creator.cs
@@ -6,53 +6,59 @@ using ExerciseOne;
 
 namespace Library
 {
-         public class Creator
-         {
-                  public static World world;
-                  public void Create()
-                  {
-                           //variable constante con el nombre del archivo
-                           const String fileName = @"test.xml";
-                           /// <summary>
-                           /// Punto de entrada
-                           /// </summary>
-                           String path = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), fileName);
-                           UriBuilder builder = new UriBuilder("file", "", 0, path);
-                           String uri = builder.Uri.ToString();
-                           // Creamos un nuevo descargador pasándole una ubicación.
-                           Downloader downloader = new Downloader(uri);
-                           // Pedimos al descargador que descargue el contenido
-                           string content;
-                           //Carga el contenido del archivo en la variable content
-                           content = downloader.Download();
+    public class Creator
+    {
+        public static World world;
+        public void Create()
+        {
+            const String fileName = @"test.xml";
+            string content = getContent(fileName);
 
-                           //Busca las tags y sus correspondientes atributos
-                           Finder finder = new Finder();
-                           List<Tag> listtags = finder.Find(content);
-                           // creamos un pipeNull
-                           IPipe pipenull = new PipeNull();
+            //Busca las tags y sus correspondientes atributos
+            Finder finder = new Finder();
+            List<Tag> listtags = finder.Find(content);
+            // creamos un pipeNull
+            IPipe pipenull = new PipeNull();
 
-                           // creamos instancias de todos los filtros que nos interecen
-                           IFilterConditional filterworld = new FilterWorld();
-                           IFilterConditional filterlevel = new FilterLevel();
-                           IFilterConditional filterscreen = new FilteScreen();
-                           IFilterConditional filterbutton = new FilterButton();
-                           IFilterConditional filterimage = new FilterImage();
+            // creamos instancias de todos los filtros que nos interecen
+            IFilterConditional filterworld = new FilterWorld();
+            IFilterConditional filterlevel = new FilterLevel();
+            IFilterConditional filterscreen = new FilteScreen();
+            IFilterConditional filterbutton = new FilterButton();
+            IFilterConditional filterimage = new FilterImage();
 
-                           // creamos instancias de todos los pipeSerial que vayamos a utilizar
-                           //IPipe pipeserial7 = new PipeSerial(filtertwitter,pipenull);
+            // creamos instancias de todos los pipeSerial que vayamos a utilizar
+            //IPipe pipeserial7 = new PipeSerial(filtertwitter,pipenull);
 
-                           IPipe pipe4 = new PipeConditional(filterimage, pipenull, pipenull);
-                           IPipe pipe3 = new PipeConditional(filterbutton, pipenull, pipe4);
-                           IPipe pipe2 = new PipeConditional(filterscreen, pipenull, pipe3);
-                           IPipe pipe1 = new PipeConditional(filterlevel, pipenull, pipe2);
-                           IPipe pipe0 = new PipeConditional(filterworld, pipenull, pipe1);
+            IPipe pipe4 = new PipeConditional(filterimage, pipenull, pipenull);
+            IPipe pipe3 = new PipeConditional(filterbutton, pipenull, pipe4);
+            IPipe pipe2 = new PipeConditional(filterscreen, pipenull, pipe3);
+            IPipe pipe1 = new PipeConditional(filterlevel, pipenull, pipe2);
+            IPipe pipe0 = new PipeConditional(filterworld, pipenull, pipe1);
 
-                           foreach (Tag tag in listtags)
-                           {
-                                    pipe0.Send(tag);
-                           }
-                           // enviamos la imagen al primer pipeSerial
-                  }
-         }
+            foreach (Tag tag in listtags)
+            {
+                pipe0.Send(tag);
+            }
+            // enviamos la imagen al primer pipeSerial
+        }
+        public string getContent(string fileName)
+        {
+            //variable constante con el nombre del archivo
+            /// <summary>
+            /// Punto de entrada
+            /// </summary>
+            String path = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), fileName);
+            UriBuilder builder = new UriBuilder("file", "", 0, path);
+            String uri = builder.Uri.ToString();
+            // Creamos un nuevo descargador pasándole una ubicación.
+            Downloader downloader = new Downloader(uri);
+            // Pedimos al descargador que descargue el contenido
+            string content;
+            //Carga el contenido del archivo en la variable content
+            content = downloader.Download();
+
+            return content;
+        }
+    }
 }
