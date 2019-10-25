2e704c1a0d6cf8a32537c71ec6eee32d70db6401
SebaColina <sebacoli00@gmail.com>
2019-10-24 22:14:40 -0300

    arreglo motor 3

--- SegundaEntrega/src/Library/Motor/MotorLvl1.cs
+++ SegundaEntrega/src/Library/Motor/MotorLvl1.cs
@@ -4,40 +4,45 @@ using System.Collections.Generic;
 
 namespace Library
 {
-    public class MotorLvl1 : IObservable
-    {
-        private List<IObserver> observers = new List<IObserver>();
-        public void Check(Letter letter)
-        {
-            if (letter.Right)
-            {
-                letter.Screen.levelCompleted();
-                foreach(IObserver observer in observers)
-                {
-                    observer.Update();
-                }
-            }
-            else
-            {
-                //mensaje de error que le erro.
-            }
-        }
+         public class MotorLvl1 : IObservable
+         {
+                  private List<IObserver> observers = new List<IObserver>();
+                  public void Check(Letter letter)
+                  {
+                           if (letter.Right)
+                           {
+                                    this.NextLevel(letter);
+                           }
+                           else
+                           {
+                                    //mensaje de error que le erro.
+                           }
+                  }
 
-        public void Subscribe(IObserver observer)
-        {
-            if (! observers.Contains(observer))
-            {
-                observers.Add(observer);
-            }
-        }
+                  public void NextLevel(Letter letter)
+                  {
+                           letter.Screen.levelCompleted();
+                           foreach (IObserver observer in observers)
+                           {
+                                    observer.Update();
+                           }
+                  }
 
-        public void Unsubscribe(IObserver observer)
-        {
-            if (observers.Contains(observer))
-            {
-                this.observers.Remove(observer);
-            }
-        }
+                  public void Subscribe(IObserver observer)
+                  {
+                           if (!observers.Contains(observer))
+                           {
+                                    observers.Add(observer);
+                           }
+                  }
 
-    }
+                  public void Unsubscribe(IObserver observer)
+                  {
+                           if (observers.Contains(observer))
+                           {
+                                    this.observers.Remove(observer);
+                           }
+                  }
+
+         }
 }
--- SegundaEntrega/src/Library/Motor/MotorLvl3.cs
+++ SegundaEntrega/src/Library/Motor/MotorLvl3.cs
@@ -7,15 +7,11 @@ namespace Library
          public class MotorLvl3 : IObservable
          {
                   private List<ButtonCheck> correctList = new List<ButtonCheck>();
-                  private int ClickNum; 
+                  private int ClickNum;
                   private List<IObserver> observers = new List<IObserver>();
                   public void Check(ButtonCheck buttonCheck)
                   {
-                           ClickNum += 1;
-                           if (buttonCheck.Check)
-                           {
-                                    this.correctList.Add(buttonCheck);
-                           }
+                           this.AddButtonCheck(buttonCheck);
                            if ((ClickNum % 2) == 0)
                            {
                                     if (this.correctList.Count == 2)
@@ -23,13 +19,22 @@ namespace Library
                                              buttonCheck.Screen.levelCompleted();
                                              foreach (IObserver observer in this.observers)
                                              {
-                                                    observer.Update();
+                                                      observer.Update();
                                              }
                                     }
                                     else
                                     {
-                                        this.correctList.Clear();
+                                             this.correctList.Clear();
                                     }
+                           }
+                  }
+
+                  public void AddButtonCheck(ButtonCheck buttonCheck)
+                  {
+                           ClickNum += 1;
+                           if (buttonCheck.Check)
+                           {
+                                    this.correctList.Add(buttonCheck);
                            }
                   }
                   public void Subscribe(IObserver observer)
