47dd0bc925f2fe45c189b6d6ebbefa93ef888065
SebaColina <sebacoli00@gmail.com>
2019-10-26 15:32:32 -0300

    Comentarios y prolijidad

--- SegundaEntrega/src/Library/Motor/MotorLvl1.cs
+++ SegundaEntrega/src/Library/Motor/MotorLvl1.cs
@@ -2,11 +2,29 @@
 using Proyecto.Common;
 using System.Collections.Generic;
 
+/// <summary>
+/// NOMBRE: MotorLvl1
+/// 
+/// DESCRIPCION: Motor encargado de la logica del nivel 1
+/// 
+/// SRP: la unica responsabilidad de esta clase es hacer la logica del nivel 1, su unica razon de cambio es modificar
+/// la logica del nivel.
+/// 
+/// COLABORACIONES: colabora con la interfaz IObserver ya que conoce una lista de IObservers, colabora con la interfaz
+/// IObservable ya que es de tipo IObservable, Colabora con la clase Letter ya que es el elemento con el que va a 
+/// realizar la logica.
+/// </summary>
+
 namespace Library
 {
          public class MotorLvl1 : IObservable
          {
                   private List<IObserver> observers = new List<IObserver>();
+
+                  /// <summary>
+                  /// checkea que sea la letra correcta
+                  /// </summary>
+                  /// <param name="letter"> letra la cual fue clickeada </param>
                   public void Check(Letter letter)
                   {
                            if (letter.Right)
@@ -19,6 +37,10 @@ namespace Library
                            }
                   }
 
+                  /// <summary>
+                  /// metodo que establece que la pantalla fue superada y se lo notifica al Observer
+                  /// </summary>
+                  /// <param name="letter"> letra que fue clickeada </param>
                   public void NextLevel(Letter letter)
                   {
                            letter.Screen.levelCompleted();
@@ -28,6 +50,10 @@ namespace Library
                            }
                   }
 
+                  /// <summary>
+                  /// metodo que agrega un IObserver a la lista de Observers
+                  /// </summary>
+                  /// <param name="observer"> observer a agregar </param>
                   public void Subscribe(IObserver observer)
                   {
                            if (!observers.Contains(observer))
@@ -36,6 +62,10 @@ namespace Library
                            }
                   }
 
+                  /// <summary>
+                  /// metodo que elimina un IObserver de la lista de Observers
+                  /// </summary>
+                  /// <param name="observer"> observer a eliminar </param>
                   public void Unsubscribe(IObserver observer)
                   {
                            if (observers.Contains(observer))
